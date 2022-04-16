bc1a9c9342f5dba1128bad8f612076aca6a184d2
Kimiato <853447650@qq.com>
2022-04-17 02:45:12 +0800

    feat: update user

--- model/user.go
+++ model/user.go
@@ -1,11 +1,22 @@
 package model
 
 import (
+	"time"
+
 	"gorm.io/gorm"
+)
+
+const (
+	Admin = iota
+	Normal
 )
 
 type User struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email"`
+	Name           string `json:"name"`
+	Email          string `json:"email"`
+	LastLogin      time.Time
+	PermissionType int8
+	Password       string `json:"password"`
+	Token          string `json:"token"`
 }
