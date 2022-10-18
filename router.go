623463f62b15fab8d2d02709432438e120bedc11
Kimiato <853447650@qq.com>
2022-10-18 17:49:46 +0800

    feat: 增加服务状态监控

--- router.go
+++ router.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"myecho/handler/view"
 )
 
 func SetupViewRouter(app *fiber.App) {
 	ViewRoute := app.Group("")
+	{
+		app.Get("/status", monitor.New()) // 监控
+	}
 	{
 		ViewRoute.Get("", view.ArticleDisplayList)
 		ViewRoute.Get("/articles/:id", view.ArticleRetrieve)
